635e195b66d7a33b2fc6b291f3361b9b51630b0e
Marcos <marcos.diez.garcia@gmail.com>
2013-06-27 10:30:07 +0200

    [27-06-13] Some examples changed. Quotes removed from empty lists

--- tests.lisp
+++ tests.lisp
@@ -21,7 +21,9 @@
 (setq a1 (make-action 'A1 (list p q) (list p q)))
 (setq a2 (make-action 'A2 (list notq) (list p)))
 
-(setq a3 (make-action 'A3 (list p q) '()))
-(setq a4 (make-action 'A4 '() (list p q)))
+(setq a3 (make-action 'A3 (list p q) ()))
+(setq a4 (make-action 'A4 () (list p q)))
 
-(setq a5 (make-action 'A5 '() '()))
+(setq a5 (make-action 'A5 () ()))
+(setq a6 (make-action 'A6 () (list notp)))
+(setq a7 (make-action 'A7 (list notq) ()))
